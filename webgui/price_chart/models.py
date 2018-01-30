8cca759026fcf115515909a50a1d18739e9f0215
Jan Schmidt <jamidt@users.noreply.github.com>
2018-01-30 20:04:32 +0100

    Added model for shapeshift

--- webgui/price_chart/models.py
+++ webgui/price_chart/models.py
@@ -9,3 +9,15 @@ class BitcoinDe(models.Model):
     tid = models.PositiveIntegerField(primary_key=True)
 
 
+class ShapeshiftMarketinfo(models.Model):
+    trading_pair = models.CharField(max_length=6)
+    date = models.DateTimeField()
+    rate = models.DecimalField(max_digits=16, decimal_places=10)
+    minerfee = models.DecimalField(max_digits=16, decimal_places=10)
+
+
+class ShapeshiftTransactions(models.Model):
+    trading_pair = models.CharField(max_length=6)
+    date = models.DateTimeField()
+    amount = models.DecimalField(max_digits=16, decimal_places=10)
+
